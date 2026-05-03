b799a760bb22d8963ecc115a11a2307667c793df
Eduardo <aersouza@gmail.com>
2026-05-03 12:49:57 -0300

    feat: setup inicial do ambiente

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-# Inicializamos a nossa API
-app = FastAPI(title="API de Agendamento de Aulas")
-
-# Criamos a nossa primeira Rota
-@app.get("/")
-def ola_mundo():
-    return {"mensagem": "Olá! A API de Agendamento de Aulas está online!"}
